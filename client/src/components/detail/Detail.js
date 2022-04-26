6fc6d4955fd424eec65b3eaf143dee60c6fd4002
Virginia <virginia.carmuega@gmail.com>
2022-04-26 10:33:42 -0300

    finished detail page

--- client/src/components/detail/Detail.js
+++ client/src/components/detail/Detail.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux';
-import './detail.css'
+import './detail.css';
+import food from '../../img/food.jpg'
 
-// Paso a paso
 
 const Detail = (props) => {
     const {id} = useParams()
@@ -11,64 +11,73 @@ const Detail = (props) => {
     let directions;
 
     if(typeof(recipe.directions) ==='string'){
-      directions = recipe.directions.split('*')
+      if(recipe.directions.length > 1){
+        directions = recipe.directions.split('*')
+      } else {directions = []}
+      
     } else {
       directions = recipe.directions;
     }
     
   
     return (
-    <div>
+    <div className='detail'>
+      <div className='detail__titleImgSummary-container'>
         <div className='detail__container'>
-          <img className='detail__image' src={recipe.image} alt={recipe.name} />
-          <h3 className='detail__title'>{recipe.title}</h3>
-          <div className='detail__underline'></div>
-        </div>
-        <div className='detail__summary-container'>
-          <h2 className='detail__subtitle'>Summary</h2>
-          <p className='detail__summary' dangerouslySetInnerHTML={{__html: recipe.summary}}></p>
-        </div>
-
+            <img className='detail__image' src={recipe.image ? recipe.image : food} alt={recipe.name} />
+            <div>
+              <h3 className='detail__title'>{recipe.title}</h3>
+              <div className='detail__underline'></div>
+            </div>
+          </div>
+          <div className='detail__summary-container'>
+            <h2 className='detail__subtitle'>Summary</h2>
+            <p className='detail__summary' dangerouslySetInnerHTML={{__html: recipe.summary}}></p>
+          </div>
+      </div>
+        
         <div className='detail__underline'></div>
 
-        <div className='detail__typesPointsContainer'>
-          <div className='detail__typesPointsContainer-individual'>
-            <h2 className='detail__subtitle'>Diet types</h2>
-            <ul>
-              {recipe.diets.map((el,idx) =>{return (
-                <li key={el.name + idx}>{el.name}</li>
-              )})}
-            </ul>
+        <div className='detail__extrainfo-container'>
+          <div className='detail__typesPointsContainer'>
+            <div className='detail__typesPointsContainer-individual'>
+              <h2 className='detail__subtitle'>Diet types</h2>
+              <ul>
+                {recipe.diets.map((el,idx) =>{return (
+                  <li key={el.name + idx}>{el.name}</li>
+                )})}
+              </ul>
+            </div>
+            <div className='detail__typesPointsContainer-individual'>
+              <h2 className='detail__subtitle'>Dish types</h2>
+              <ul>
+                {recipe.dishTypes ? recipe.dishTypes.map(el =>{return (
+                  <li key={el}>{el}</li>
+                )}) : <li>Dish type not avalible</li>}
+              </ul>
+            </div>
           </div>
-          <div className='detail__typesPointsContainer-individual'>
-            <h2 className='detail__subtitle'>Dish types</h2>
-            <ul>
-              {recipe.dishTypes ? recipe.dishTypes.map(el =>{return (
-                <li key={el}>{el}</li>
-              )}) : <li>Dish type not avalible</li>}
-            </ul>
+
+          <div className='detail__typesPointsContainer'>
+            <div className='detail__typesPointsContainer-individual'>
+              <h2 className='detail__subtitle'>Score</h2>
+              <p className='detail__p'>{recipe.score}</p>
+            </div>
+            <div className='detail__typesPointsContainer-individual'>
+              <h2 className='detail__subtitle'>Health Score</h2>
+              <p className='detail__p'>{recipe.healthScore}</p>
+            </div>
           </div>
         </div>
 
-        <div className='detail__underline'></div>
-
-        <div className='detail__typesPointsContainer'>
-          <div className='detail__typesPointsContainer-individual'>
-            <h2 className='detail__subtitle'>Score</h2>
-            <p className='detail__p'>{recipe.score}</p>
-          </div>
-          <div className='detail__typesPointsContainer-individual'>
-            <h2 className='detail__subtitle'>Health Score</h2>
-            <p className='detail__p'>{recipe.healthScore}</p>
-          </div>
-        </div>
+        
 
         <div className='detail__underline'></div>
 
         <div className='detail__summary-container'>
         <h2 className='detail__subtitle'>Directions</h2>
-        {directions.length > 0 ? directions.map((el,idx) =>{ return (
-          <div key={`step${idx+1}`} className={!el.length && 'hide'}>
+        {directions.length > 1 ? directions.map((el,idx) =>{ return (
+          <div key={`step${idx+1}`}>
             <p className='detail__p'>Step {idx+1}:</p>
             <div className='detail__directions-steps' >
               <span >{el}</span>
