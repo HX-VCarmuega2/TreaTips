1719b92d65eebed9a8e4a114493af0b2027757c1
Virginia <virginia.carmuega@gmail.com>
2022-04-26 10:27:46 -0300

    displayers components ready y responsive

--- client/src/components/display/Display.js
+++ client/src/components/display/Display.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './display.css';
+import food from '../../img/food.jpg'
 
 const Display = ({recipe}) => {
   const {title,image,diets,id } = recipe;
   
     return (
-    <div>
-        <h3>{title}</h3>
-        <img src={image} alt={title} />
-        {diets.map((diet,idx)=>{return(
-        <li key={idx}>{diet.name}</li>)}
-        )}
-        <Link to={`/recipe/${id}`}>
+    <div className='recipe__container'>
+        <img className='recipe__image' src={image ? image : food} alt={title} />
+        <h3 className='recipe__title'>{title}</h3>
+        <div className='recipe__underline'></div>
+        <div className='recipe__container-dietsType'>
+          {diets.map((diet,idx)=>{return(
+          <li className='recipe__dietType' key={idx}>{diet.name}</li>)}
+          )}
+        </div>
+        <Link className='recipe__link' to={`/recipe/${id}`}>
           <p>Reed more</p>
         </Link>
     </div>
--- client/src/components/displayer/Displayer.js
+++ client/src/components/displayer/Displayer.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import Display from '../display/Display'
 import { connect } from 'react-redux';
+import './displayer.css'
 
 
 const Displayer = (props) => {
     
   return (
-    props.recipes.map((recipe)=>{return(
-      <Display key={recipe.id} recipe={recipe} />
-    )})
+    <div className={props.recipes.length > 0 ? 'displayer' : 'hide'}>
+      <div>
+        <h3 className='displayer__title'>Recipes</h3>
+        <div className='displayer__underline'></div>
+      </div>
+      <div className='displayer__display-container'>
+        {props.recipes.map((recipe)=>{return(
+          <Display key={recipe.id} recipe={recipe}/>
+        )})}
+      </div>
+  </div>
   )
 }
+
 function mapStateToProps(state){
   return {
     recipes: state.recipesToDisplay
