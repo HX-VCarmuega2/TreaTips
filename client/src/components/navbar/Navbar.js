dea2d845185edee077aae97ecf00c734e465111b
Virginia <virginia.carmuega@gmail.com>
2022-04-30 14:46:10 -0300

    Improved Navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,38 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import './navbar.css'
-import lupa from '../../img/lupa.png';
-import { getRecipesByName } from '../../redux/actions';
+import Search from '../search/Search';
 
-
-const Navbar = (props) => {
-  const [word,setWord] = useState('');
-
-  const handleSubmit = ()=>{
-    props.getRecipesByName(word)
-    setWord("")
-  }
-
+const Navbar = () => {
   return (
     <div className='navbar__container'>
       <div className='navbar__container-title'>
         <h2 className='navbar__title'>TreaTips</h2>
         <div className="navbar__underline"></div>
       </div>
-      <div className='navbar__search'>
-        <input
-          type="text" 
-          value={word}
-          placeholder='e.g: chicken wings' 
-          onChange={(e)=>setWord(e.target.value)} />
-        <button 
-          type='submit' 
-          onClick={handleSubmit}>
-            <img src={lupa} alt="search button"/>
-        </button>
-      </div>
-        
+      <Search />
       <div className='navbar__container-links'>
         <Link className='navbar__link' to="/home">Home</Link>
         <Link className='navbar__link' to="/post">Create</Link>
@@ -41,10 +19,4 @@ const Navbar = (props) => {
   )
 }
 
-function mapDispatchToProps(dispatch){
-  return {
-    getRecipesByName:(word)=> dispatch(getRecipesByName(word))
-  }
-}
-
-export default connect(null,mapDispatchToProps)(Navbar)
+export default Navbar
---
+++ client/src/components/search/Search.js
@@ -0,0 +1,37 @@
+import React, { useState }from 'react'
+import '../navbar/navbar.css'
+import { connect } from 'react-redux';
+import { getRecipesByName } from '../../redux/actions';
+import lupa from '../../img/lupa.png';
+
+const Search = (props) => {
+    const [word,setWord] = useState('');
+    
+    const handleSubmit = ()=>{
+    props.getRecipesByName(word)
+    setWord("")
+  }
+
+  return (
+    <div className='navbar__search'>
+        <input
+          type="text" 
+          value={word}
+          placeholder='e.g: chicken wings' 
+          onChange={(e)=>setWord(e.target.value)} />
+        <button 
+          type='submit' 
+          onClick={handleSubmit}>
+            <img src={lupa} alt="search button"/>
+        </button>
+      </div>
+  )
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+      getRecipesByName:(word)=> dispatch(getRecipesByName(word))
+    }
+  }
+  
+  export default connect(null,mapDispatchToProps)(Search)
