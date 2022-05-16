c6332a6321b5d7d0c632cd1e685c326fb502fc34
Virginia <virginia.carmuega@gmail.com>
2022-05-16 15:14:24 -0300

    homePage: change connect for useDispatch and useSelector

--- client/src/pages/homePage/HomePage.js
+++ client/src/pages/homePage/HomePage.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Navbar from '../../components/navbar/Navbar'
 import Pagination from '../../components/pagination/Pagination';
 import Filter from '../../components/filters/Filter';
 import Displayer from '../../components/displayer/Displayer';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllRecipes } from '../../redux/actions';
 import Modal from '../../components/modal/Modal';
 import error from '../../img/errorConexion.jpg';
 import './homePage.css'
@@ -13,20 +14,25 @@ import './homePage.css'
 const title = 'Ups!'
 const msg = 'It seems we are having technical problems. Please try again in a few minutes'
 
-const HomePage = (props) => {
+const HomePage = () => {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   
-
+  useEffect(() => {
+    dispatch(getAllRecipes())
+  },[dispatch]);  
+  
+  const state = useSelector((state)=> state);
 
   return (
     <div>
         <Navbar />
-        {props.loading ? (
+        {state.loading ? (
           <div className='loading__container'>
             <h3 className='loading__msg'>Loading Recipes...</h3>
-          </div>) : (props.error ? (
-            <Modal title={title} msg={msg} img={error} closeModal={()=>navigate('/')} />):
+          </div>) : (state.errors.request ? (
+            <Modal title={title} msg={msg} img={error} closeModal={()=>{navigate('/')}} />):
           <div>
             <Filter />
             <Pagination />
@@ -37,10 +43,5 @@ const HomePage = (props) => {
   )
 }
 
-function mapStateToProps(state){
-  return {
-    loading: state.loading,
-    error: state.errors.request
-  }; 
-}
-export default connect(mapStateToProps)(HomePage)
+
+export default HomePage
